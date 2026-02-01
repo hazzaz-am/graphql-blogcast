c51a98e1e371556ffc9aa93562aab797504cbfd8
hazzaz-am <hazzazabdulmannan@gmail.com>
2026-02-01 19:29:12 +0600

    feat: implement HomePage layout with BlogCast title and navigation link

--- app/page.js
+++ app/page.js
@@ -1,5 +1,18 @@
+import Link from "next/link";
+
 export default function HomePage() {
-  return (
-    <div>HomePage</div>
-  )
+	return (
+		<div className="flex relative flex-col justify-center items-center h-screen">
+			<div className="p-2 flex flex-col justify-center items-center bg-gray-100 lg:w-2/5 h-1/4 rounded">
+				<div className="flex pb-5 mb-5 text-5xl text-back">
+					<h2>BlogCast</h2>
+				</div>
+				<div>
+					<Link href="/posts" className="bg-black text-white hover:bg-black/90 p-3 rounded-sm text-lg text mr-2">
+						TAKE ME TO THE BLOG
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 }
